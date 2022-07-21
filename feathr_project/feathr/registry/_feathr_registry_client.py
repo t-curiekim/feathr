89dff86a8209d7ed83e6ec168912009668578449
Curie <t-curiekim@microsoft.com>
2022-07-21 13:12:19 -0700

    test

--- feathr_project/feathr/registry/_feathr_registry_client.py
+++ feathr_project/feathr/registry/_feathr_registry_client.py
@@ -189,7 +189,7 @@ class _FeatureRegistry(FeathrRegistry):
         return check(requests.post(f"{self.endpoint}{path}", headers=self._get_auth_header(), json=body)).json()
 
     def _get_auth_header(self) -> dict:
-        return {"Authorization": f'Bearer {self.credential.get_token(".default")}'}
+        return {"Authorization": f'Bearer {self.credential.get_token("{self.endpoint}{path}/.default")}'}
     
     @classmethod
     def _get_py_files(self, path: Path) -> List[Path]:
